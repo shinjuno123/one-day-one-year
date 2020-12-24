56307d7ee39cb720f865234074945f43f997cd71
shinjuno <shinjuno123@naver.com>
2020-12-24 12:59:03 +0900

    secondPage js and css update

--- odoyApp/static/odoyApp/secondPage.js
+++ odoyApp/static/odoyApp/secondPage.js
@@ -3,4 +3,4 @@ const progress_bar2 = document.getElementsByClassName("progress-bar2")[0];
 progress_bar1.style.setProperty('--width', 50);
 progress_bar2.style.setProperty('--width', 40);
 
-// setProperty('--width',(input percent here getting the value from django))
+// setProperty('--width',(input percentage here getting the value from django))
