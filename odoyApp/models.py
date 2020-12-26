1e7e1fd9fba732de377c4dfdad2bbf52d8629e36
JeonYoungSong <syh45678910@gmail.com>
2020-12-26 14:27:11 +0900

    models, views 구현중

--- odoyApp/models.py
+++ odoyApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class odoyUser(models.Model):
+    pub_date = models.DateTimeFieldField('date published')  # data생성일자
+    name = models.CharField(max_length = 200, help_text='닉네임을 입력하세요') # user이름
+    gender = models.BinaryField(default=0) # 성별 0은 여자, 1은 남자
+    birth = models.DateField() # 생년월일
+    life = models.IntegerField(default = 0) # 수명 없다면 0
+
+    def odoyUser(self):
+        return odoyUser
+
--- odoyApp/views.py
+++ odoyApp/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from odoyApp.models import odoyUser
+from django import forms
+import datetime
+
 
 # Create your views here.
 
-def odoymain(request):
+def odoymain(request): # 이름,날짜,성별,수명을 입력받는 함수
+    if (request.method == 'POST'):
+        user = odoyUser() #user에 odoyUser를 복사
+        user.name = request.POST['name'] #이름을 입력받는다.
+        if (request.POST['gender'] == '0'):
+            user.gender = 0
+        else:
+            user.gender = 1
+        user.birth = datetime.strptime(request.POST['birth'],'%Y-%M-%D' )# String type이기때문에 바꾸어줘야함
+
+        user.life = request.POST['life']
+
     return render(request, 'odoymain.html')
 
 
 def secondPage(request):
-    return render(request,'secondPage.html')
+    return render(request, 'secondPage.html')
