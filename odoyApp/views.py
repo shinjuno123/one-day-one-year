fa5967c65cfb01ac0709c6cfa8b47ba0aa888366
shinjuno <shinjuno123@naver.com>
2020-12-28 21:37:38 +0900

    odoyMain modified and backend support

--- odoyApp/views.py
+++ odoyApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def odoymain(request):
@@ -7,4 +7,12 @@ def odoymain(request):
 
 
 def secondPage(request):
+    if(request.method == 'POST'):
+        nickname = request.POST['nickname']
+        ngender = request.POST['gender']
+        nbirth = request.POST['birth']
+        nlife = request.POST['life']
+        new_data = User_Info(name = nickname,gender = ngender,birth = nbirth,life = nlife)
+        new_data.save()
+        print(nickname + " " + ngender + " " + nbirth + " " + nlife)
     return render(request,'secondPage.html')
