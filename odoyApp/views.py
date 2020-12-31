1412c6c840daf4b23089c9fa643d3904195df03b
DongGyu1996 <ldk1362@naver.com>
2021-01-01 02:08:13 +0900

    테스트

--- odoyApp/views.py
+++ odoyApp/views.py
@@ -6,17 +6,13 @@ def odoymain(request):
     return render(request, 'odoymain.html')
 
 
-def resultpage(request):
+def secondPage(request):
     if(request.method == 'POST'):
         nickname = request.POST['nickname']
-
-        if request.POST['gender']=="0":
-            ngender = 'female'
-        else:
-            ngender = 'male'
+        ngender = request.POST['gender']
         nbirth = request.POST['birth']
         nlife = request.POST['life']
         new_data = User_Info(name = nickname,gender = ngender,birth = nbirth,life = nlife)
         new_data.save()
         print(nickname + " " + ngender + " " + nbirth + " " + nlife)
-    return render(request,'resultpage.html')
+    return render(request,'secondPage.html')
