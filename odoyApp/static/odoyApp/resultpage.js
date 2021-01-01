7c33c3f832205e5b99789aa18d98a11f3bcad860
shinjuno <shinjuno123@naver.com>
2021-01-01 16:29:44 +0900

    animation added in share-screen and progress bar

--- odoyApp/static/odoyApp/resultpage.js
+++ odoyApp/static/odoyApp/resultpage.js
@@ -1,20 +1,43 @@
 const progress_bar1 = document.getElementsByClassName("progress-bar1")[0];
 const progress_bar2 = document.getElementsByClassName("progress-bar2")[0];
-progress_bar1.style.setProperty('--width', 70);
-progress_bar2.style.setProperty('--width', 40);
-
 // setProperty('--width',(input percentage here after getting the value from django))
+
+
+
+
+function animation_progress_bar1() {
+    let bar1 = getComputedStyle(progress_bar1);
+    const bar1_width = parseFloat(bar1.getPropertyValue('--width')) || 0;
+    if (bar1_width == 70) {
+        return;
+    }
+    console.log("graph1");
+    progress_bar1.style.setProperty('--width', bar1_width + .1);
+}
+
+setInterval(animation_progress_bar1, 2);
+
+setInterval(() => {
+    let bar2 = getComputedStyle(progress_bar2);
+    const bar2_width = parseFloat(bar2.getPropertyValue('--width')) || 0;
+    if (bar2_width == 40) {
+        return;
+    }
+    console.log("graph2");
+    progress_bar2.style.setProperty('--width', bar2_width + .1);
+}, 2)
+
 
 //Draw Graph
 const graph1 = document.getElementById("myChart1").getContext('2d');
 
-const myChart1 = new Chart(graph1,{
+const myChart1 = new Chart(graph1, {
     type: 'doughnut',
-    data:{
-        labels: [ "여자", "남자"],
+    data: {
+        labels: ["여자", "남자"],
         datasets: [{
             label: '참가자(%):',
-            data: [45,55],
+            data: [45, 55],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -30,25 +53,25 @@ const myChart1 = new Chart(graph1,{
     options: {
         maintainAspectRatio: true, // default value. false일 경우 포함된 div의 크기에 맞춰서 그려짐.
         title: {
-            display : true,
+            display: true,
             text: '남,녀 참가자 비율',
-            fontSize : 20,
-            fontColor:'rgba(46,49,49,1)'
+            fontSize: 20,
+            fontColor: 'rgba(46,49,49,1)'
         },
     }
-    
+
 
 });
 
 const graph2 = document.getElementById("myChart2").getContext('2d');
-const myChart2 = new Chart(graph2,{
+const myChart2 = new Chart(graph2, {
     type: 'bar',
-    data:{
-        labels: [ "여자", "남자"],
+    data: {
+        labels: ["여자", "남자"],
         datasets: [{
-            label:'남,녀 평균 기대수명',
-            fillColor : "rgba(150,200,250,0.5)",
-            data: [85,74],
+            label: '남,녀 평균 기대수명',
+            fillColor: "rgba(150,200,250,0.5)",
+            data: [85, 74],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -64,28 +87,28 @@ const myChart2 = new Chart(graph2,{
     options: {
         maintainAspectRatio: true, // default value. false일 경우 포함된 div의 크기에 맞춰서 그려짐.
         title: {
-            display : true,
+            display: true,
             text: '남,녀 평균 기대수명',
-            fontSize : 20,
-            fontColor:'rgba(46,49,49,1)'
+            fontSize: 20,
+            fontColor: 'rgba(46,49,49,1)'
         },
         scales: {
             xAxes: [{
                 ticks: {
-                    fontColor:'rgba(40,40,40,1)'
+                    fontColor: 'rgba(40,40,40,1)'
                 }
             }],
             yAxes: [{
                 ticks: {
                     beginAtZero: true,
-                    fontColor:'rgba(40,40,40,1)'
+                    fontColor: 'rgba(40,40,40,1)'
 
                 }
             }]
         }
     }
-    
-    
+
+
 
 });
 
@@ -93,26 +116,44 @@ const myChart2 = new Chart(graph2,{
 //setBackGround
 let imagesArray = new Array('jan.jpg');
 let body = document.querySelector("body");
-let imageUrl ="url('/static/odoyApp/images/"+imagesArray[0] +"') no-repeat center fixed";
+let imageUrl = "url('/static/odoyApp/images/" + imagesArray[0] + "') no-repeat center fixed";
 body.style.background = imageUrl;
 
 
 //share-section
 let share_button = document.getElementById("share-button");
 let share_box = document.getElementById("share-box");
-share_button.addEventListener("click",function(){
+let clickTime = 0;
+share_button.addEventListener("click", function () {
 
-    if(share_box.style.display == 'block'){
-        share_box.style.transitionDuration=  "5s";
-        share_box.style.display = "none";
+    if (clickTime % 2 == 0) {
+        clickTime += 1;
+        let interval = setInterval(() => {
+            let height = getClipHeight() +3;
+            let str = "rect(0px, 250px, " + height + "px, 0px)";
+            share_box.style.clip = str;
+            if (height >= 250) {
+                clearInterval(interval);
+            }
+        }, 0.3);
+    } else {
+        clickTime += 1;
+        let interval1 = setInterval(() => {
+            let height = getClipHeight() -3;
+            let str = "rect(0px, 250px, " + height + "px, 0px)";
+            share_box.style.clip = str;
+            if (height < 0) {
+                clearInterval(interval1);
+            }
+        }, 0.05);
     }
-    else{
-        share_box.style.display = "block";
-        share_box.style.transitionDuration =  "5s";
-    }
-    // for(let i=0;i<=250;i += 0.1){
-    //     setTimeout(function(){
-    //         share_box.style.clip= "rect()";
-    //     },200);
-    // }
+
+
 });
+function getClipHeight(){
+    let style = getComputedStyle(share_box);
+    let clip_size = style.getPropertyValue("clip");
+    clip_size = clip_size.substring(5).substring(0, clip_size.length - 3);
+    let arr_size = clip_size.split("px, ");
+    return parseInt(arr_size[2]);
+}
