2359b738fbf376ddb41a388bf2bf244f65bd7172
DongGyu1996 <ldk1362@naver.com>
2021-01-11 03:24:24 +0900

    Exception Handling2

--- odoyApp/views.py
+++ odoyApp/views.py
@@ -109,9 +109,13 @@ def resultpage(request):
         
         return life
     
-    try:
+     try:
         male_life = User_Info.objects.filter(gender='male').aggregate(Avg('life'))
         male_life = temp_life(male_life)
+    except TypeError:
+        pass
+
+    try:
         female_life = User_Info.objects.filter(gender='female').aggregate(Avg('life'))
         female_life = temp_life(female_life)
     except TypeError:
